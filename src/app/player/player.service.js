88979dbccd8ac7fb61eb79989d65de9f6d89d8e4
Adam Weeks <me@adamweeks.com>
2015-02-15 22:55:18 -0500

    PlayerService tests added.

--- src/app/player/player.service.js
+++ src/app/player/player.service.js
@@ -11,24 +11,43 @@
         };
 
         /////////////
+        /**
+         * New Player Object
+         * @param playerName
+         * @param initialScore
+         * @constructor
+         */
         function Player(playerName, initialScore) {
             this.score = initialScore;
+            this.initialScore = initialScore;
             this.name = playerName;
         }
 
+        /**
+         * Change the score by amount
+         * @param amountToChange
+         */
         Player.prototype.changeScore = function(amountToChange){
             if(!angular.isDefined(this.score)){
-                this.score = 0;
+                this.score = this.initialScore;
             }
 
             this.score += amountToChange;
         };
 
+        /**
+         * Resets the score of a player back to initial score.
+         */
         Player.prototype.resetScore = function () {
-            this.score = 0;
+            this.score = this.initialScore;
         };
 
-
+        /**
+         * Creates a new player object
+         * @param playerName
+         * @param initialScore
+         * @returns {PlayerService.Player}
+         */
         function newPlayer(playerName, initialScore) {
             var player = new Player(playerName, initialScore);
             return player;
