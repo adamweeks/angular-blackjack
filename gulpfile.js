2315b2922070a87f2b1cd8fa987aa82f283b811a
Adam Weeks <me@adamweeks.com>
2015-02-23 22:11:35 -0500

    Karma test added to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var ngAnnotate = require('gulp-ng-annotate');
+var karma = require('karma').server;
 
 gulp.task('concat', function () {
     return gulp.src(['src/app/**/*.module.js', 'src/app/**/*.js', '!src/app/**/*.spec.js'])
@@ -8,3 +9,13 @@ gulp.task('concat', function () {
         .pipe(ngAnnotate())
         .pipe(gulp.dest('src/app/'));
 });
+
+gulp.task('test', function (done) {
+    karma.start({
+        configFile: __dirname + '/karma.config.js',
+        singleRun: true
+    }, done);
+});
+
+
+
