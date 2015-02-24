8a81679db62b549be3988328fd68e3e6adbabedc
Adam Weeks <me@adamweeks.com>
2015-02-23 23:19:13 -0500

    Inject app.js into index.html

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,20 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var ngAnnotate = require('gulp-ng-annotate');
 var karma = require('karma').server;
+var del = require('del');
+var inject = require('gulp-inject');
 
-gulp.task('concat', function () {
+
+//Configuration
+var buildFolder = __dirname + '/build/';
+
+
+//Tasks
+gulp.task('concat', function (done) {
     return gulp.src(['src/app/**/*.module.js', 'src/app/**/*.js', '!src/app/**/*.spec.js'])
         .pipe(concat('app.js'))
         .pipe(ngAnnotate())
-        .pipe(gulp.dest('src/app/'));
+        .pipe(gulp.dest(buildFolder), done);
 });
 
 gulp.task('test', function (done) {
@@ -17,5 +25,19 @@ gulp.task('test', function (done) {
     }, done);
 });
 
+gulp.task('index',['clean'],function(){
+    return gulp.src('./src/index.html')
+        .pipe(gulp.dest('./build'));
+});
 
+gulp.task('inject', ['clean', 'concat', 'index'], function(){
+    return gulp.src('./build/index.html')
+        .pipe(inject(gulp.src('./build/app.js', {read: false}), {relative: true}))
+        .pipe(gulp.dest('./build'));
+});
 
+gulp.task('clean', function(done) {
+    del(buildFolder + '**', done);
+});
+
+gulp.task('build', ['clean', 'concat']);
